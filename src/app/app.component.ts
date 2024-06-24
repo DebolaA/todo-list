91f1be63aae912fa3d651ce9c6b1d9b610e50c99
Debola Adedapo <niikee80@hotmail.com>
2024-06-24 19:30:11 +0100

    todo list service created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { EndpointService } from './services/endpoint.service';
+import { ITodoItem } from './interfaces/todo-item.dt';
+import { TodoList } from './interfaces/todo-list.dt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList: ITodoItem[] = TodoList;
+
+  constructor(private endpointService: EndpointService) {
+    this.endpointService.setTodoList(this.todoList);
+    console.log(this.todoList);
+  }
 }
---
+++ src/app/services/endpoint.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { ITodoItem } from '../interfaces/todo-item.dt';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EndpointService {
+  private readonly todoList$ = new BehaviorSubject<Array<ITodoItem>>([]);
+
+  setTodoList(todoList: Array<ITodoItem>): void {
+    this.todoList$.next(todoList);
+  }
+
+  getTodoList(): Observable<Array<ITodoItem>> {
+    return this.todoList$.asObservable();
+  }
+
+  createTodoItem(item: ITodoItem): Observable<boolean> {
+    let todoList: ITodoItem[] = this.todoList$.value;
+    if (todoList) {
+      todoList.push(item);
+      this.todoList$.next(todoList);
+      return of(true);
+    }
+    return of(false);
+  }
+
+  deleteUser(item: ITodoItem): Observable<boolean> {
+    let todoList: ITodoItem[] = this.todoList$.value;
+    if (item) {
+      const newList = todoList.filter(
+        (x: ITodoItem) =>
+          x.todoTitle.toLowerCase() !== item.todoTitle.toLowerCase()
+      );
+      this.todoList$.next(newList);
+      return of(true);
+    }
+    return of(false);
+  }
+
+  updateUser(item: ITodoItem): Observable<boolean> {
+    let todoList: ITodoItem[] = this.todoList$.value;
+    if (todoList) {
+      const index = todoList.findIndex(
+        (x: ITodoItem) =>
+          x.todoTitle.toLowerCase() === item.todoTitle.toLowerCase()
+      );
+      if (index > -1) todoList[index] = item;
+      this.todoList$.next(todoList);
+      return of(true);
+    }
+    return of(false);
+  }
+}
