725bdf88d7a080e4799c81a8a5cf68f162a03577
Debola Adedapo <niikee80@hotmail.com>
2024-06-25 15:40:15 +0100

    deleting unwanted code

--- src/app/directives/directive.module.ts
+++ src/app/directives/directive.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BckgndColorDirective } from './bckgnd-color.directive';
-import { HoverDirective } from './hover.directive';
 import { TooltipDirective } from './tooltip.directive';
 
 @NgModule({
-  declarations: [BckgndColorDirective, HoverDirective, TooltipDirective],
+  declarations: [BckgndColorDirective, TooltipDirective],
   imports: [CommonModule],
   exports: [BckgndColorDirective, TooltipDirective],
 })
